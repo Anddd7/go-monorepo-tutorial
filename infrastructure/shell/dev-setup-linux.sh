15e018280cf8e0266c62aa10aead6f1deefdd9bb
Anddd7 <liaoad_sapce@sina.com>
2022-09-02 16:27:05 +0800

    install terraform

--- infrastructure/shell/dev-setup-linux.sh
+++ infrastructure/shell/dev-setup-linux.sh
@@ -10,3 +10,27 @@ else
     sudo install skaffold /usr/local/bin/skaffold
     rm skaffold
 fi
+
+echo "0. install terraform"
+
+FILE=/usr/bin/terraform
+if test -f "$FILE"; then
+    echo "$FILE exists."
+else 
+    sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
+    
+    wget -O- https://apt.releases.hashicorp.com/gpg | \
+    gpg --dearmor | \
+    sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+
+    gpg --no-default-keyring \
+    --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
+    --fingerprint
+
+    echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+    https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+    sudo tee /etc/apt/sources.list.d/hashicorp.list
+
+    sudo apt update
+    sudo apt-get install terraform
+fi
