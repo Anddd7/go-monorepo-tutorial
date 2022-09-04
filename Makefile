4641cc21296ca8a77f7741430081cce08be6fcf4
Anddd7 <liaoad_sapce@sina.com>
2022-09-04 21:40:46 +0800

    refactor manifest generator

--- Makefile
+++ Makefile
@@ -2,6 +2,7 @@ GIT_REVISION 		:= $(shell git rev-parse --verify --short HEAD 2>/dev/null)
 COMPILE_TIME 		:= $(shell git log -1 --format="%ad" --date=short)
 ENV 				?= local-windows
 
+build: export TARGET_ENV=${ENV}
 build:
 	skaffold build -p ${ENV}
 	./infrastructure/shell/argocd_push.sh 'in ${GIT_REVISION} at ${COMPILE_TIME}'
--- infrastructure/make/skaffold.mk
+++ infrastructure/make/skaffold.mk
@@ -8,8 +8,7 @@ GIT_REVISION		:= ${VAR_GIT_REVISION}
 # calculated variables
 
 DEPLOY_DIR 			:= ${RELATIVE_PATH}/deploy
-MANIFEST_FOLDER 	:= ${GIT_ROOT_DIR}/artifact/argocd/${ENV}/services/${SERVICE_NAME}
-MANIFEST_FILE 		:= ${MANIFEST_FOLDER}/k8s.yaml
+MANIFEST_FOLDER 	:= ${GIT_ROOT_DIR}/artifact/argocd/${TARGET_ENV}/services
 
 # required environment variables
 
@@ -23,12 +22,10 @@ manifest-version:
 dry-run: export IMAGE=${PROJECT_IDENTIFIER}/${SERVICE_NAME}:${GIT_REVISION}-locally
 dry-run: manifest-version docker-local
 	@echo ">> dry-run the kubernetes specs"
-	kustomize build ${DEPLOY_DIR}/overlays/${ENV} | envsubst
+	kustomize build ${DEPLOY_DIR}/overlays/${TARGET_ENV} | envsubst
 
 manifest: export VERSION=${COMPILE_TIME}-${GIT_REVISION}
 manifest: manifest-version docker
 	@echo ">> dry-run the kubernetes specs"
-	echo ${SERVICE_NAME}
-	echo ${ENV}
-#	mkdir -p ${MANIFEST_FOLDER}
-#	kustomize build ${DEPLOY_DIR}/overlays/${ENV} | envsubst >| ${MANIFEST_FILE}
+	mkdir -p ${GIT_ROOT_DIR}/artifact/argocd/${TARGET_ENV}/services/${SERVICE_NAME}
+	kustomize build ${DEPLOY_DIR}/overlays/${TARGET_ENV} | envsubst >| ${MANIFEST_FOLDER}/${SERVICE_NAME}/k8s.yaml
