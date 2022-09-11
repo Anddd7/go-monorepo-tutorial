59b0a1ec2d7ee56afc41bd01a3902eb8bdfab1f4
Anddd7 <liaoad_sapce@sina.com>
2022-09-11 17:06:32 +0800

    load image to minikube

--- infrastructure/shell/docker_build.sh
+++ infrastructure/shell/docker_build.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
 docker build -t "$IMAGE" "$BUILD_CONTEXT"
+
+# docker push
+
+if [[ $IMAGE == *"locally" ]];then
+    echo "this image is build locally and load into minikube ..."
+    minikube image load $IMAGE
+fi
