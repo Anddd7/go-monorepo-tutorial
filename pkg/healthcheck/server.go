c46eaaad6ac7511d7a9e18f5ade7463df490a790
Anddd7 <liaoad_sapce@sina.com>
2022-09-11 20:19:58 +0800

    fix: fatal log

--- pkg/healthcheck/server.go
+++ pkg/healthcheck/server.go
@@ -15,12 +15,12 @@ func RegisterHealthServer(s *grpc.Server) {
 type server struct{}
 
 func (s *server) Check(ctx context.Context, in *this.HealthCheckRequest) (*this.HealthCheckResponse, error) {
-	log.Fatalln("Health Checking ... ... ...")
+	log.Printf("Health Checking ... ... ...")
 	return &this.HealthCheckResponse{Status: this.HealthCheckResponse_SERVING}, nil
 }
 
 func (s *server) Watch(in *this.HealthCheckRequest, w this.Health_WatchServer) error {
-	log.Fatalln("Health Checking ... ... ... (Watching)")
+	log.Printf("Health Checking ... ... ... (Watching)")
 	r := &this.HealthCheckResponse{Status: this.HealthCheckResponse_SERVING}
 	for {
 		w.Send(r)
