b04a5534d0c0bbf2d86046c969b7d5b37490d6ce
Anddd7 <liaoad_space@sina.com>
2022-11-11 16:11:43 +0800

    fix misc errors

--- scripts/shell/grpc_gen.sh
+++ scripts/shell/grpc_gen.sh
@@ -6,6 +6,7 @@ services_path=services
 cd "${root_dir}/${services_path}" || exit
 
 protoc -I. \
+    --proto_path=. \
     --go_out=paths=source_relative:. \
     --go-grpc_out=require_unimplemented_servers=false,paths=source_relative:. \
     ./**/*.proto
